163146f098a1b2b1b935ec8ff22f5ea0c2b6ab87
Guilhermesilvaandre <guilhermeaandre2018@gmail.com>
2025-05-26 21:53:46 -0300

    Add o cod do tradutor de libras

--- Sites/Site otimizado/Comofeito.php
+++ Sites/Site otimizado/Comofeito.php
@@ -40,6 +40,36 @@ fazendo
 </body>
 </html>
 
+
+<!-- <div vw class="enabled">                               cod do tradutor de libras
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <!-- Aumenta o tamanho da fonte da pag
     
     <style>
